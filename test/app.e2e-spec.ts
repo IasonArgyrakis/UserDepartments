d3cc1482f74d443d381da0d26468762e00632753
Iason Argyrakis <jasonargirakis@gmail.com>
2023-02-11 01:16:19 +0200

    ci/cd

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,16 +5,9 @@ import {
 import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
-import {
-  AuthDto,
-  RegisterAuthDto,
-} from '../src/auth/dto';
-import {
-  CreateDepartmentDto,
-  UpdateDepartmentDto,
-} from '../src/department/dto';
+import { RegisterAuthDto } from '../src/auth/dto';
+
 import { PrismaService } from '../src/prisma/prisma.service';
-import { EditUserDto } from '../src/user/dto';
 
 function test_without_each_key(original) {
   const prop_names = Object.keys(original);
@@ -40,7 +33,6 @@ function test_segment(
 ) {
   describe(name, () => {
     test_without_each_key(model);
-    console.log({ ...model });
     it(`should ${name}`, () => {
       return pactum
         .spec()
@@ -192,8 +184,7 @@ describe('App e2e', () => {
             lastName: user_2.lastName,
             firstName: user_2.firstName,
             afm: user_2.afm,
-          })
-          .inspect();
+          });
       });
     });
 
@@ -213,8 +204,7 @@ describe('App e2e', () => {
             lastName: user_2.lastName,
             firstName: user_2.firstName,
             afm: user_2.afm,
-          })
-          .inspect();
+          });
       });
     });
   });
@@ -275,6 +265,7 @@ describe('App e2e', () => {
         .expectStatus(403);
     });
   });
+
   describe('Departments', () => {
     describe('Get empty departments', () => {
       it('should get departments', () => {
@@ -301,8 +292,7 @@ describe('App e2e', () => {
           })
           .withBody(departmments[0])
           .expectStatus(201)
-          .stores('department_1', '')
-          .inspect();
+          .stores('department_1', '');
       });
 
       it('create department with same name should fail', () => {
@@ -387,193 +377,4 @@ describe('App e2e', () => {
       });
     });
   });
-
-  //
-  // describe('Get User_2', () => {
-  //   it('should get current user', () => {
-  //     return pactum
-  //       .spec()
-  //       .get('/users/me')
-  //       .withHeaders({
-  //         Authorization:
-  //           'Bearer $S{user_1_signin.access_token}',
-  //       })
-  //       .expectStatus(200)
-  //       .inspect();
-  //   });
-  // });
-  //
-  // describe('Edit user', () => {
-  //   it('should edit user', () => {
-  //     const dto: EditUserDto = {
-  //       firstName: 'Other',
-  //       email: 'jason@gmail.com',
-  //     };
-  //     return pactum
-  //       .spec()
-  //       .patch('/users/{id}')
-  //       .withPathParams(
-  //         'id',
-  //         '$S{currentUser.id}',
-  //       )
-  //       .withHeaders({
-  //         Authorization: 'Bearer $S{userAt}',
-  //       })
-  //       .withBody(dto)
-  //       .expectStatus(200);
-  //   });
-  // });
-
-  //
-
-  //
-
-  //
-  // describe('Edit department by id', () => {
-  //   it('should edit department by id', () => {
-  //     const dto: UpdateDepartmentDto = {
-  //       title: 'Accounting & Logistics',
-  //     };
-  //     return pactum
-  //       .spec()
-  //       .patch('/departments/{id}')
-  //       .withPathParams('id', '$S{department.id}')
-  //       .withHeaders({
-  //         Authorization: 'Bearer $S{userAt}',
-  //       })
-  //       .withBody(dto)
-  //       .expectStatus(200)
-  //       .expectBodyContains('$S{department.id}')
-  //       .inspect();
-  //   });
-  // });
-  //
-  // describe('Add user to department ', () => {
-  //   it('should add user to department', () => {
-  //     const dto: UpdateDepartmentDto = {
-  //       title: 'Accounting & Logistics',
-  //     };
-  //     return pactum
-  //       .spec()
-  //       .patch('/departments/{id}/add/{userId}')
-  //       .withPathParams('id', '$S{department.id}')
-  //       .withPathParams(
-  //         'userId',
-  //         '$S{currentUser.id}',
-  //       )
-  //       .withHeaders({
-  //         Authorization: 'Bearer $S{userAt}',
-  //       })
-  //       .withBody(dto)
-  //       .expectStatus(200)
-  //       .expectBodyContains('$S{department.id}')
-  //       .inspect();
-  //   });
-  // });
-
-  // describe('Bookmarks', () => {
-  //   describe('Get empty bookmarks', () => {
-  //     it('should get bookmarks', () => {
-  //       return pactum
-  //         .spec()
-  //         .get('/bookmarks')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .expectStatus(200)
-  //         .expectBody([]);
-  //     });
-  //   });
-  //
-  //   describe('Create bookmark', () => {
-  //     const dto: CreateBookmarkDto = {
-  //       title: 'First Bookmark',
-  //       link: 'https://www.youtube.com/watch?v=d6WC5n9G_sM',
-  //     };
-  //     it('should create bookmark', () => {
-  //       return pactum
-  //         .spec()
-  //         .post('/bookmarks')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .withBody(dto)
-  //         .expectStatus(201)
-  //         .stores('bookmarkId', 'id');
-  //     });
-  //   });
-  //
-  //   describe('Get bookmarks', () => {
-  //     it('should get bookmarks', () => {
-  //       return pactum
-  //         .spec()
-  //         .get('/bookmarks')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .expectStatus(200)
-  //         .expectJsonLength(1);
-  //     });
-  //   });
-  //
-  //   describe('Get bookmark by id', () => {
-  //     it('should get bookmark by id', () => {
-  //       return pactum
-  //         .spec()
-  //         .get('/bookmarks/{id}')
-  //         .withPathParams('id', '$S{bookmarkId}')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .expectStatus(200)
-  //         .expectBodyContains('$S{bookmarkId}');
-  //     });
-  //   });
-  //
-  //   describe('Edit bookmark by id', () => {
-  //     const dto: EditBookmarkDto = {
-  //       title:
-  //         'Kubernetes Course - Full Beginners Tutorial (Containerize Your Apps!)',
-  //       description:
-  //         'Learn how to use Kubernetes in this complete course. Kubernetes makes it possible to containerize applications and simplifies app deployment to production.',
-  //     };
-  //     it('should edit bookmark', () => {
-  //       return pactum
-  //         .spec()
-  //         .patch('/bookmarks/{id}')
-  //         .withPathParams('id', '$S{bookmarkId}')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .withBody(dto)
-  //         .expectStatus(200)
-  //         .expectBodyContains(dto.title)
-  //         .expectBodyContains(dto.description);
-  //     });
-  //   });
-  //
-  //   describe('Delete bookmark by id', () => {
-  //     it('should delete bookmark', () => {
-  //       return pactum
-  //         .spec()
-  //         .delete('/bookmarks/{id}')
-  //         .withPathParams('id', '$S{bookmarkId}')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .expectStatus(204);
-  //     });
-  //
-  //     it('should get empty bookmarks', () => {
-  //       return pactum
-  //         .spec()
-  //         .get('/bookmarks')
-  //         .withHeaders({
-  //           Authorization: 'Bearer $S{userAt}',
-  //         })
-  //         .expectStatus(200)
-  //         .expectJsonLength(0);
-  //     });
-  //   });
-  // });
 });
