d9abdde9acd0664571638d227f23e8707d0636ba
Iason Argyrakis <jasonargirakis@gmail.com>
2023-02-11 02:13:24 +0200

    swagger fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
-  SwaggerModule,
   DocumentBuilder,
+  SwaggerModule,
 } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
